429d9433f2d892b0822bda2532efa5c549f66dd9
fujikawa <fujikawa.naoki@mac.com>
2018-04-11 19:30:47 +0900

    FIX DESIGN

--- App.js
+++ App.js
@@ -1,129 +1,215 @@
 import React from 'react';
 import {
-  StyleSheet,
-  Dimensions,
-  Text,
-  View,
-  SafeAreaView
+    StyleSheet,
+    Dimensions,
+    Text,
+    View,
+    SafeAreaView
 } from 'react-native';
 import {
-  Video
+    Video
 } from 'expo';
 
 import {
-  Col,
-  Row,
-  Grid
+    Col,
+    Row,
+    Grid
 } from "react-native-easy-grid";
 
 import {
-  FormLabel,
-  ButtonGroup,
-  Button
-} from 'react-native-elements'
+    FormLabel,
+    ButtonGroup,
+    Button
+} from 'react-native-elements';
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'gray',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  rightContainer: {
-    flex: 1,
-    backgroundColor: 'gray',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  leftContainer: {
-    flex: 1,
-    backgroundColor: 'gray',
-    alignItems: 'left',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: 'gray',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    rightContainer: {
+        flex: 1,
+        backgroundColor: 'gray',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    leftContainer: {
+        flex: 1,
+        backgroundColor: 'gray',
+        //alignItems: 'left',
+        justifyContent: 'center',
+    }
 });
 
 
 export default class App extends React.Component {
 
-  constructor(props, context, updater) {
-    super(props, context, updater);
+    constructor(props, context, updater) {
+        super(props, context, updater);
 
-    this.state = {
-      mute: false,
-      shouldPlay: true,
-      volume: 1.0,
-      //uri: "http://192.168.1.2/streaming/mystream.m3u8",
-      uri: "http://qthttp.apple.com.edgesuite.net/1010qwoeiuryfg/sl.m3u8",
-      //uri: 'http://techslides.com/demos/sample-videos/small.mp4',
-      isFull: false,
-      id: null,
-      name: null,
-      sex: null,
-      age: null
-    };
+        this.state = {
+            mute: false,
+            shouldPlay: true,
+            volume: 1.0,
+            //uri: "http://192.168.1.2/streaming/mystream.m3u8",
+            uri: "http://localhost:8080/streaming/mystream.m3u8",
+            //uri: "http://qthttp.apple.com.edgesuite.net/1010qwoeiuryfg/sl.m3u8",
+            //uri: 'http://techslides.com/demos/sample-videos/small.mp4',
+            id: null,
+            name: null,
+            sex: null,
+            age: null,
+            cnt: "  ",
+            selected0Index: null,
+            selected1Index: 0,
+            selected2Index: null,
+            selected3Index: 0
+        };
 
-    Expo.ScreenOrientation.allow(Expo.ScreenOrientation.Orientation.LANDSCAPE);
-  }
+        this.update0Index = this.update0Index.bind(this);
+        this.update1Index = this.update1Index.bind(this);
+        this.update2Index = this.update2Index.bind(this);
+        this.update3Index = this.update3Index.bind(this);
 
-  render() {
+        Expo.ScreenOrientation.allow(Expo.ScreenOrientation.Orientation.LANDSCAPE);
+    }
 
-    let {width, height} = Dimensions.get('window');
 
-    console.log("window.width", width);
-    console.log("window.height", height);
+    update0Index(selected0Index) {
+        if (this.state.selected0Index === null) {
+            this.setState({
+                selected0Index: 0,
+                id: 1234567890,
+                name: "東京　太郎",
+                sex: "男",
+                age: 40,
+                cnt: " 1"
+            })
+        } else {
+            this.setState({
+                selected0Index: null,
+                id: null,
+                name: null,
+                sex: null,
+                age: null,
+                cnt: "  "
+            })
+        }
+    }
 
-    width = width * 0.8;
-    height = 960 * (width / 1520);
+    update1Index(selected1Index) {
+        this.setState({selected1Index})
+    }
 
-    console.log("video_width", width);
-    console.log("video_height", height);
+    update2Index(selected2Index) {
+        this.state.selected2Index === null ? this.setState({selected2Index: 0}) : this.setState({selected2Index: null})
 
-    return (
-      <SafeAreaView style={styles.container}>
-        <Grid>
-          <Col size={20}>
-            {/*<View style={styles.leftContainer}>*/}
-              {/*<Row>*/}
+    }
 
-              {/*</Row>*/}
-              {/*<Row>*/}
-                {/*<Text>2</Text>*/}
-              {/*</Row>*/}
-              {/*<Row><Text>3</Text></Row>*/}
-              {/*<Row><Text>4</Text></Row>*/}
-              {/*<Row><Text>5</Text></Row>*/}
-              {/*<Row><Text>6</Text></Row>*/}
-              {/*<Row><Text>7</Text></Row>*/}
-              {/*<Row><Text>8</Text></Row>*/}
-              {/*<Row>*/}
-                {/*<ButtonGroup*/}
-                  {/*//onPress={this.updateIndex}*/}
-                  {/*selectedIndex={"1"}*/}
-                  {/*buttons={["1","2","3","4"]}*/}
-                  {/*containerStyle={{height: 100}}*/}
-                {/*/>*/}
-              {/*</Row>*/}
-            {/*</View>*/}
-          </Col>
-          <Col size={80}>
-            <View style={styles.rightContainer}>
-              <Video
-                source={{uri: this.state.uri}}
-                shouldPlay={this.state.shouldPlay}
-                resizeMode={Expo.Video.RESIZE_MODE_CONTAIN}
-                rate={1.0}
-                volume={this.state.volume}
-                style={{width, height}}
-                isMuted={this.state.mute}
-                useNativeControls={true}
-              />
+    update3Index(selected3Index) {
+        this.setState({selected3Index})
+    }
+
+    render() {
+
+        let {width, height} = Dimensions.get('window');
+
+        console.log("window.width", width);
+        console.log("window.height", height);
+
+        width = width * 0.8 * 0.9;
+        height = 960 * (width / 1520);
+
+        console.log("video_width", width);
+        console.log("video_height", height);
+
+        return (
+            <View style={styles.container}>
+                <Grid>
+                    <Col size={20}>
+                        <View style={styles.leftContainer}>
+                            <Row>
+                                <ButtonGroup
+                                    textStyle={{color: "white"}}
+                                    onPress={this.update0Index}
+                                    selectedIndex={this.state.selected0Index}
+                                    buttons={["患者情報取得"]}
+                                    //containerStyle={{height: 35, width: 150, borderRadius: 10, backgroundColor: "gray"}}
+                                />
+                            </Row>
+                            <Row>
+                                <FormLabel
+                                    //labelStyle={{color: "white", fontSize: 22, width: 180}}
+                                >
+                                    ID:{this.state.id}
+                                </FormLabel>
+                            </Row>
+                            <Row>
+                                <FormLabel
+                                //    labelStyle={{color: "white", fontSize: 22, width: 180}}
+                                >氏名:{this.state.name}</FormLabel>
+                            </Row>
+                            <Row>
+                                <FormLabel
+                                    //labelStyle={{color: "white", fontSize: 22, width: 100}}
+                                >年齢:{this.state.age}</FormLabel>
+                            </Row>
+                            <Row>
+                                <FormLabel
+                                //    labelStyle={{color: "white", fontSize: 22, width: 100}}
+                                >性別:{this.state.sex}</FormLabel>
+                            </Row>
+                            <Row>
+                                <FormLabel
+                                //    labelStyle={{color: "white", fontSize: 22, width: 100}}
+                                >{this.state.cnt}枚</FormLabel>
+                            </Row>
+                            <Row>
+                                <ButtonGroup
+                                    onPress={this.update1Index}
+                                    selectedIndex={this.state.selected1Index}
+                                    buttons={['記録', '取消', '終了']}
+                                    //containerStyle={{height: 100}}
+                                />
+                            </Row>
+                            <Row>
+                                <ButtonGroup
+                                    onPress={this.update2Index}
+                                    selectedIndex={this.state.selected2Index}
+                                    buttons={["Voice"]}
+                                    //containerStyle={{height: 100}}
+                                />
+                            </Row>
+                            <Row>
+                                <ButtonGroup
+                                    onPress={this.update3Index}
+                                    selectedIndex={this.state.selected3Index}
+                                    buttons={["1", "2", "3", "4"]}
+                                    //containerStyle={{height: 100}}
+                                />
+                            </Row>
+                        </View>
+                    </Col>
+                    <Col size={80}>
+                        <View style={styles.rightContainer}>
+                            <Video
+                                source={{uri: this.state.uri}}
+                                shouldPlay={this.state.shouldPlay}
+                                resizeMode={Expo.Video.RESIZE_MODE_CONTAIN}
+                                rate={1.0}
+                                volume={this.state.volume}
+                                style={{width, height}}
+                                isMuted={this.state.mute}
+                                useNativeControls={true}
+                            />
+                        </View>
+                    </Col>
+                </Grid>
             </View>
-          </Col>
-        </Grid>
-      </SafeAreaView>
-    );
-  }
+        );
+    }
 }
 
 
